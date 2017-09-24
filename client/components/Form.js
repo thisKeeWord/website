385854cccb33d4cbae0fef17c7f0759a5d74bebd
Leonard Kee <lkee@Leonards-MacBook-Pro.local>
2017-09-23 17:55:11 -0700

    forms and stuff

--- client/components/Form.js
+++ client/components/Form.js
@@ -8,22 +8,26 @@ class PostForm extends React.Component {
   //   })
   // }
   post(writing) {
-    $.post('', writing, (error, success) {
+    $.post('', writing, (error, success) => {
       console.log('success again');
     });
   }
 
   testing(e) {
     e.preventDefault();
-    let entry = $(".formEntry").text();
-    this.post(entry).done(() => {
-      console.log('successful post sent');
-    })
+    console.log(this.post)
+    let entry = { 
+      entry: $(".formEntry").text() 
+    };
+    this.post(entry)
+    // .done(() => {
+      // console.log('successful post sent');
+    // });
   }
 
   render() {
     return (
-      <form onSubmit={this.testing}>
+      <form onSubmit={this.testing.bind(this)}>
         <div className="formEntry" role="textbox" placeholder="What's poppin?" contentEditable="true" aria-multiline="true" spellCheck="true">
         </div>
         <input type="submit" value="submit"></input>
--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
 var fallback = require('express-history-api-fallback');
-var User = require('./../database/userModel');
-var userController = require('./../database/userController');
+// var User = require('./../database/userModel');
+// var userController = require('./../database/userController');
 var mongoose = require('mongoose');
 // var mongoURI = process.env.NODE_ENV === 'test1' ? 'mongodb://localhost/test1' : 'mongodb://localhost/gainsApp';
 // mongoose.connect(mongoURI);
@@ -22,9 +22,18 @@ app.get('/', function(req, res) {
 });
 
 app.post('/fitness', function(req, res) {
-	console.log('test')
-	res.send(['hello'])
+
+	console.log(req.body)
+	res.send(['hello']);
 });
+
+app.post('/blogs', function(req, res) {
+	res.send('asdf');
+});
+
+app.post('/projects', function(req, res) {
+	res.send('asdf');
+})
 // app.post('/login', userController.verifyUser);
 
 
