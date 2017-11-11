a3eea3409d44c41d3f418003d353cbf0ffc4f9da
Leonard Kee <leonardwkee@gmail.com>
2017-11-11 03:01:46 -0800

    dropdown menu edits

--- client/components/Navigate.js
+++ client/components/Navigate.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router';
-// import Misc from './Misc';
+import { NavDropdown, MenuItem } from 'react-bootstrap';
 
 class Nav extends React.Component {
 	// constructor() {
@@ -14,21 +14,27 @@ class Nav extends React.Component {
 		
 	// }
 
+ // <div id="menuToggle">
+ //          <input type="checkbox" />
+ //          <span></span>
+ //          <span></span>
+ //          <span></span>
+
   render() {
     return (
       <div className="Explore">
-        <div id="menuToggle">
-          <input type="checkbox" />
-          <span></span>
-          <span></span>
-          <span></span>
-        	<ul id="linkToPages">
-            <li id="goHome"><Link to="/">Home</Link></li>
-            <li id="goFitness"><Link to="/fitness">Fitness</Link></li>
-            <li id="goBlogs"><Link to="/blogs">Blogs</Link></li>
-            <li id="goProjects"><Link to="/projects">Projects</Link></li>
-          </ul>
-        </div>
+        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+          <Link to="/">Home</Link>
+          <Link to="/fitness">Fitness</Link>
+          <Link to="/blogs">Blogs</Link>
+          <Link to="/projects">Projects</Link>
+        </NavDropdown>
+      	<ul id="linkToPages">
+          <li id="goHome"><Link to="/">Home</Link></li>
+          <li id="goFitness"><Link to="/fitness">Fitness</Link></li>
+          <li id="goBlogs"> <Link to="/blogs">Blogs</Link></li>
+          <li id="goProjects"><Link to="/projects">Projects</Link></li>
+        </ul>
       </div>
     )
   }
