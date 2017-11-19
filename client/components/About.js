8ab283802ab784acda9a1cb27e9e986fb1a5bd99
Leonard Kee <leonardwkee@gmail.com>
2017-11-18 16:33:04 -0800

    done

--- client/components/About.js
+++ client/components/About.js
@@ -43,6 +43,32 @@ class About extends React.Component {
             </div>
           </div>
         </div>
+        <div className="whatIDo">
+            "Wake up determined. Go to bed satisfied. And somewhere in between eat a cookie.." - Dwayne Johnson
+            <br/>
+            <br/>
+            There are people that might not like Rocky himself, but the quote hits the mark and his drive to be the best.
+            <br/>
+            <br/>
+            I pursued mathematics in college, started lifting weights and still do to this day, played my share of videogames and watched many tv shows and movies,
+            and even considered training to become a professional League of Legends player.
+            <br/>
+            <br/>
+            During my pursuit of different possible avenues, I found coding to be more interesting.
+            Furthermore, as one who ponders and thinks a decent amount, blogging allows me to share the knowledge
+            I have gained over the years with others.
+            <br/>
+            <br/>
+            As I continue to pursue my dreams - though those dreams my change over time - the one thing I won't stop
+            doing is 1-up'ing myself. 
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            "I fight for a better tomorrow." - Jayce (League of Legends)
+            <br/>
+            "Hardly a challenge." - Illidan Stormrage (Warcraft III)
+          </div>
         <Footers />
       </div>
     )
