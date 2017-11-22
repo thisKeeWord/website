73ae6e1f42f13be330817df26ecb2fd23a80c907
Leonard Kee <leonardwkee@gmail.com>
2017-11-21 20:18:01 -0800

    css update

--- client/components/Navigate.js
+++ client/components/Navigate.js
@@ -7,12 +7,20 @@ class Navigate extends React.Component {
     const that = this;
     return (
       <div className="Explore">
-        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-          <Link to="/">Home</Link>
-          <Link to="/fitness">Fitness</Link>
-          <Link to="/blogs">Blogs</Link>
-          <Link to="/projects">Projects</Link>
-        </NavDropdown>
+        <div className="viewDropdown">
+          <span className="briefHeading">
+            Leonard Kee | Developer | Powerbuilder | Blogger
+          </span>
+          <NavDropdown title="Menu" id="basic-nav-dropdown">
+            <li className="menuLinks">
+              <Link to="/">Home</Link>
+              <Link to="/fitness">Fitness</Link>
+              <Link to="/blogs">Blogs</Link>
+              <Link to="/projects">Projects</Link>
+            </li>
+          </NavDropdown>
+        </div>
+
         <Nav bsStyle="tabs" activeKey={location.pathname.split('/')[1]}>
           <NavItem id="navTabs" eventKey="" href="/">Home</NavItem>
           <NavItem id="navTabs" eventKey="fitness" href="/fitness">Fitness</NavItem>
