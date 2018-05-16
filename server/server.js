097204e5a33a57fce62461eb0c52880fa2004e20
Leonard Kee <lkee@Leonards-MacBook-Pro.local>
2018-05-15 23:44:43 -0700

    new

---
+++ server/server.js
@@ -0,0 +1,30 @@
+
+var fs = require('fs');
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var fallback = require('express-history-api-fallback');
+
+
+app.set('port', (process.env.PORT || 3001));
+
+// decoding data as string for format for url
+// app.use(bodyParser.json({limit: '500mb'}));
+// app.use(bodyParser.urlencoded({limit: '500mb', extended: true}));
+app.use(express.static(path.join(__dirname, './../')));
+// app.use(fallback('index.html', { root: __dirname + './../client/' }));
+
+app.get('/', function(req, res) {
+    res.sendFile('index.html');
+});
+
+
+
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
+module.exports = app;
