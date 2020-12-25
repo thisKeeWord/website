e75725864e8c1bf9fc0107a76f0f88c8c00008f4
thisKeeWord <leonardwkee@gmail.com>
2020-12-25 01:59:37 -0800

    removed unused variables and comments, audited packages

--- server/server.js
+++ server/server.js
@@ -1,19 +1,10 @@
-
-var fs = require('fs');
 var express = require('express');
 var app = express();
 var path = require('path');
-var bodyParser = require('body-parser');
-var fallback = require('express-history-api-fallback');
-
 
 app.set('port', (process.env.PORT || 3001));
 
-// decoding data as string for format for url
-// app.use(bodyParser.json({limit: '500mb'}));
-// app.use(bodyParser.urlencoded({limit: '500mb', extended: true}));
 app.use(express.static(path.join(__dirname, './../')));
-// app.use(fallback('index.html', { root: __dirname + './../client/' }));
 
 app.get('/', function(req, res) {
   res.sendFile('index.html');
@@ -22,9 +13,6 @@ app.get('/', function(req, res) {
 app.get('/resume', function(req, res) {
   res.sendFile(path.join(__dirname, '../secondary_pages', 'resume.html'));
 });
-
-
-
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
