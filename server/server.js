55110953d54ff52186628b2542481e42fdc256db
thisKeeWord <leonardwkee@gmail.com>
2021-04-29 14:37:53 -0700

    remove space

--- server/server.js
+++ server/server.js
@@ -6,17 +6,16 @@ app.set('port', (process.env.PORT || 3001));
 
 app.use(express.static(path.join(__dirname, './../')));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile('index.html');
 });
 
-app.get('/resume', function(req, res) {
+app.get('/resume', function (req, res) {
   res.sendFile(path.join(__dirname, '../secondary_pages', 'resume.html'));
 });
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
-
 
 module.exports = app;
